780530c59d9f7275d5b6d5552f131c210fcaba5f
Litoz <67237108+Liyot@users.noreply.github.com>
2026-03-27 12:00:28 +0100

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import NavBar from "@/app/components/navBar";
+import Header from "@/app/components/Header";
+
+export default function Home() {
+  return (
+   <div >
+     <Header/>
+   </div>
+  );
+}
